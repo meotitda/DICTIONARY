03bb2f9bc70dc19aa8a469ae0ea358735936b958
MoonSupport <jiwon3346@naver.com>
2020-06-13 13:46:22 +0900

    copyDA

---
+++ copyDA.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Copy Dir Address
+
+function takePrefix() {
+    PREFIX=${FILE:0:1}
+    UPPER_PREFIX=`echo $PREFIX | tr '[:lower:]' '[:upper:]'`
+    echo $UPPER_PREFIX
+}
+
+function takeWithoutPrefix() {
+    REMAINDER=${FILE:1}
+    LOWER_REMAINDER=`echo $REMAINDER | tr '[:upper:]' '[:lower:]'`
+    echo $LOWER_REMAINDER
+}
+
+
+if [ -z ${1} ]; then
+    echo '[Error] Invalid File Name'
+    exit 1
+fi
+
+
+FILE=$1
+
+PREFIX=$(takePrefix)
+REMAINDER=$(takeWithoutPrefix)
+TITLE=$PREFIX$REMAINDER
+
+DIRECTORY=./$PREFIX/$TITLE.md
+
+echo $DIRECTORY | pbcopy
