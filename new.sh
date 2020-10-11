cc1f9ce8f667d0860cc4448c696e6c43c6483189
MoonSupport <jiwon3346@naver.com>
2020-10-11 15:56:37 +0900

    new script 수정

--- new.sh
+++ new.sh
@@ -34,22 +34,65 @@ fi
 touch $DIRECTORY
 
 cat > ${DIRECTORY} <<- EOM
+<d-title>
+
 # ${TITLE}
+
+</d-title>
+
+
+<d-label>
+
+<d-inner>
+
+![Common](../2TAT1C/Label_Common.png)
+
+</d-inner>
+
+</d-label>
+
+<d-origin>
 
 원형 : 입력
 
+</d-origin>
+
+<d-mean>
+
 의미  : <span style="color:#FFBF00; font-weight:bold;">강조</span>
 
+</d-mean>
+
+<d-pronunciation>
+
 발음 : 알리아스
+
+</d-pronunciation>
+
+<d-content>
 
 설명
 ([참고](주소))
 
+</d-content>
+
+<d-relation>
+
 ## 관련 기술
+
+<d-inner>
+
 1. 예시 1
+
+</d-inner>
+
+<d-inner>
 
 2. 예시 2
 
+</d-inner>
+
+</d-relation>
 
 EOM
 
