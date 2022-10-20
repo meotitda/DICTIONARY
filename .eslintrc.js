c25ba8ee1455d69dbad967bb247ccce7fc60e73c
MoonSupport <jiwon3346@naver.com>
2022-10-21 00:08:23 +0900

    [fix] 버전 리버트

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-   "root": true,
-   "env": {
-       "es2021": true
-   },
-   "extends": [
-       "eslint:recommended",
-       "plugin:@typescript-eslint/recommended"
-   ],
-   "overrides": [
-   ],
-   "parserOptions": {
-       "ecmaVersion": "latest"
-   },
-   "plugins": [
-   ],
-   "rules": {
-       "semi": ["error", "always"],
-       "quotes": ["error", "double"]
-   }
+  root: true,
+  env: {
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: [],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["warn", "double"],
+  },
 };
