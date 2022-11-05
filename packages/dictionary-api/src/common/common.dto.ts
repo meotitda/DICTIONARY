4353b264203f3b0fcbe64424985ab92fd61ba5b3
서민혁 <mahns1201@xingxingmobility.com>
2022-11-06 01:33:57 +0900

    [del] date dto

--- packages/dictionary-api/src/common/common.dto.ts
+++ packages/dictionary-api/src/common/common.dto.ts
@@ -1,19 +1,3 @@
-import { IsDate, IsOptional } from 'class-validator';
-
-export class DateDto {
-  @IsDate()
-  @IsOptional()
-  createdAt: Date;
-
-  @IsDate()
-  @IsOptional()
-  updatedAt: Date;
-
-  @IsDate()
-  @IsOptional()
-  deletedAt: Date;
-}
-
 export interface ResultDto {
   items: any;
   message: string;
