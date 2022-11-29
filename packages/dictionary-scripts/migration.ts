692282b8048362a7992a35a9853c73c89ef39f4f
서민혁 <mahns1201@gmail.com>
2022-11-30 02:58:42 +0900

    [feat] error handling

--- packages/dictionary-scripts/migration.ts
+++ packages/dictionary-scripts/migration.ts
@@ -37,77 +37,80 @@ const WordSchema = new Schema<MigrationIWord>({
 const WordModel = mongoose.model("Word", WordSchema);
 
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URI);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
 
-  const dbWords = await WordModel.find({ deletedAt: null }).exec();
-  const mdWords = runner(ROOT);
-  const dbWordTable = dbWords.reduce<Map<string, any>>((map, next) => {
-    const { _id, slug, title: key, labels, tags, body } = next;
-    const value = {
-      _id,
-      slug,
-      labels,
-      tags,
-      body,
-    };
-    map.set(key, value);
-    return map;
-  }, new Map());
-  const mdWordTitles = [];
-  const mdWordTable = mdWords.reduce<Map<string, any>>((map, next) => {
-    const [[key, value]] = Object.entries(next);
-    map.set(key, value);
-    mdWordTitles.push(key);
-    return map;
-  }, new Map());
+    const dbWords = await WordModel.find({ deletedAt: null }).exec();
+    const mdWords = runner(ROOT);
+    const dbWordTable = dbWords.reduce<Map<string, any>>((map, next) => {
+      const { _id, slug, title: key, labels, tags, body } = next;
+      const value = {
+        _id,
+        slug,
+        labels,
+        tags,
+        body,
+      };
+      map.set(key, value);
+      return map;
+    }, new Map());
+    const mdWordTitles = [];
+    const mdWordTable = mdWords.reduce<Map<string, any>>((map, next) => {
+      const [[key, value]] = Object.entries(next);
+      map.set(key, value);
+      mdWordTitles.push(key);
+      return map;
+    }, new Map());
 
-  await Promise.all(
-    mdWordTitles.map(async (mdTitle) => {
-      const dbWord = dbWordTable.get(mdTitle);
-      const { slug, title, labels, tags, body } = mdWordTable.get(mdTitle);
+    await Promise.all(
+      mdWordTitles.map(async (mdTitle) => {
+        const dbWord = dbWordTable.get(mdTitle);
+        const { slug, title, labels, tags, body } = mdWordTable.get(mdTitle);
 
-      if (!dbWord) {
-        await new WordModel({
-          slug,
-          title,
-          labels,
-          tags,
-          body,
-          createdAt: new Date(),
-        }).save();
+        if (!dbWord) {
+          await new WordModel({
+            slug,
+            title,
+            labels,
+            tags,
+            body,
+            createdAt: new Date(),
+          }).save();
 
-        console.log(`${title} is registered`);
-      } else {
-        const { labels: dbLabels, tags: dbTags, body: dbBody } = dbWord;
+          console.log(`${title} is registered`);
+        } else {
+          const { labels: dbLabels, tags: dbTags, body: dbBody } = dbWord;
 
-        const diff = {};
-        if (JSON.stringify(labels) !== JSON.stringify(dbLabels)) {
-          diff[DIFF.LABELS] = labels;
+          const diff = {};
+          if (JSON.stringify(labels) !== JSON.stringify(dbLabels)) {
+            diff[DIFF.LABELS] = labels;
+          }
+          if (JSON.stringify(tags) !== JSON.stringify(dbTags)) {
+            diff[DIFF.TAGS] = tags;
+          }
+          if (body !== dbBody) {
+            diff[DIFF.BODY] = body;
+          }
+
+          if (Object.keys(diff).length > 0) {
+            await WordModel.findOneAndUpdate(
+              { title },
+              {
+                labels: diff[DIFF.LABELS] || dbLabels,
+                tags: diff[DIFF.TAGS] || dbTags,
+                body: diff[DIFF.BODY] || dbBody,
+                updatedAt: new Date(),
+              }
+            ).exec();
+          }
         }
-        if (JSON.stringify(tags) !== JSON.stringify(dbTags)) {
-          diff[DIFF.TAGS] = tags;
-        }
-        if (body !== dbBody) {
-          diff[DIFF.BODY] = body;
-        }
-
-        if (Object.keys(diff).length > 0) {
-          await WordModel.findOneAndUpdate(
-            { title },
-            {
-              labels: diff[DIFF.LABELS] || dbLabels,
-              tags: diff[DIFF.TAGS] || dbTags,
-              body: diff[DIFF.BODY] || dbBody,
-              updatedAt: new Date(),
-            }
-          ).exec();
-        }
-      }
-    })
-  );
-
-  console.log("MONGO LOCKED 🔒");
-  await mongoose.disconnect();
+      })
+    );
+  } catch (error) {
+    throw new Error(error);
+  } finally {
+    await mongoose.disconnect();
+  }
 }
 
 main();
